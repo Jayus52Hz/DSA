8a3b6c1dcd33e0dc338a8db52c9e49d6546e0bae
DOCUTEE <quangforwork1203@gmail.com>
2023-11-13 20:36:29 +0700

    Bo sung function doc file va di kem file txt

--- scripts.js
+++ scripts.js
@@ -12,4 +12,19 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+      if(rawFile.readyState === 4)  {
+        if(rawFile.status === 200 || rawFile.status == 0) {
+          var allText = rawFile.responseText;
+          console.log(allText);
+         }
+      }
+    }
+    rawFile.send(null);
+}
+readTextFile("./words_alpha.txt")
