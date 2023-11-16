da8ce7af7a14a17a45cb4725504aad03a3b67774
DOCUTEE <110187691+DOCUTEE@users.noreply.github.com>
2023-11-16 00:25:17 -0800

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,28 +1,8 @@
-
 class Player {
     constructor(name) {
         this.name = name;
     }
     sendMessage() {
-        var check = false;
-        while (check === false) {
-            document.getElementById("user-message").addEventListener('keydown', function (event) {
-                if (event.key === 'enter') {
-                    var messageInput = document.getElementById("user-message");
-                    var message = messageInput.value.toLowerCase();
-                    if (message.trim() !== "") {
-                        var exist = dictionary.findString(message);
-                        if (exist === true) {
-                            var messageElement = document.createElement('div');
-                            var messageContainer = document.getElementById("message-container");
-                            messageElement.textContent = message;
-                            messageContainer.append(messageElement);
-                            check = true;
-                        }
-                    }
-                    else messageInput.value = "";
-                }
-            });
-        }
+        return document.getElementById('user-message').value;
     }
 }
