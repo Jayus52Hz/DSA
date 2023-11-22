63e0c192c91a910f7e7a0719492d2c9a49f20035
DOCUTEE <110187691+DOCUTEE@users.noreply.github.com>
2023-11-22 11:09:50 -0700

    Update Quizgame.js

--- Quizgame.js
+++ Quizgame.js
@@ -1,39 +1,100 @@
 var quangdb = dictionary;
-function check(x, ans){
-      if (x== 0 && ans =="A"){
-      console.log("Right"); return true;}
-      else if (x == 1 && ans == "B"){
-      console.log("Right"); return true }
-      else if (x == 2 && ans == "C"){
-      console.log("Right"); return true;}
-      else if (x == 3 && ans == "D"){
-      console.log("Right"); return true;}
-      else {console.log("Wrong"); return false;}
+var timesChallenge = 3;
+function check(x, ans) {
+      if (x== 0 && ans =="A") return true;
+      else if (x == 1 && ans == "B") return true;
+      else if (x == 2 && ans == "C") return true;
+      else if (x == 3 && ans == "D") return true;
+      else return false;
 }
 var arrqq = []; 
+var times = 0;
+var x;
+var arr = [];
+var maximum =0;
+var count = 0;
+function resetPractice(){
+      document.getElementById("practice-container").textContent = '';
+}
+function createQuestion(){
+      arr = [];
+      for(var i = 0 ; i < 4; i++)                  
+      { 
+            arr.push(quangdb.aWordStartWith(String.fromCharCode(rand(97,122))));
+      }
+      x = rand(0,3);
+      arrqq.push(arr[x]);
+      addDivToContainer("Mean: " + quangdb.getMean(arr[x]) + " ?","practice-container","quesion");
+      addDivToContainer("A: " + arr[0],"practice-container","choice");
+      addDivToContainer("B: " + arr[1],"practice-container","choice");
+      addDivToContainer("C: " + arr[2],"practice-container","choice");
+      addDivToContainer("D: " + arr[3],"practice-container","choice");
+}
+function setTime(){
+      times = parseInt (window.prompt("Ban muon choi bao nhieu lan ?"));
+}
+var modePractice = 1;
 function learn(){
-      var times = parseInt (window.prompt("Times?"));
-      while (times-- > 0){
-            var arr = [];
-            for(var i = 0 ; i<4; i++)
-            { arr.push(quangdb.aWordStartWith(String.fromCharCode(rand(97,122))));
+      modePractice = 1;
+      resetPractice();
+      setTime();
+      createQuestion();
+}
+//1: learn
+//2: challenge
+//3: test
+document.getElementById("user-reply").addEventListener("keydown",function(event){
+      if (event.key == "Enter"){
+            console.log(modePractice);
+            var ans = document.getElementById("user-reply").value; 
+            if (modePractice == 1){
+                  if (times > 0)
+                  {
+                        if (ans != ""){
+                              times--;
+                              printResult(check(x,ans));
+                              createQuestion();
+                        }
+                  }                  
+                  if (times == 0) window.alert("End game!");
             }
-            var x = rand(0,3);
-            arrqq.push(arr[x]);
-            console.log("Nghia:" + quangdb.getMean(arr[x]));
-            console.log("A: " + arr[0]);
-            console.log("B: " + arr[1]);
-            console.log("C: " + arr[2]);
-            console.log("D: " + arr[3]);
-            var ans = window.prompt(" ");
-            check(x, ans); 
+            else 
+            if (modePractice == 2){
+                  if (timesChallenge > 0){
+                        if (ans!= ""){
+                              var temp = check(x,ans);
+                              count += temp;
+                              timesChallenge += temp-1;
+                              console.log(count,timesChallenge);
+                              printResult(temp);
+                              createQuestion();
+                        }
+                  }
+                  if (timesChallenge == 0)
+                  {
+                        if (count > maximum) {
+                              window.alert("You lost ! New record is " + count);
+                              maximum = count;
+                        }
+                        else window.alert("You lost ! Your score is " + count);           
+                  }      
+            }
+            document.getElementById("user-reply").value = '';
+      }
+});
+
+function printResult(check){
+      if (check) {
+            addDivToContainer(arr[x] + " is correct", "practice-container", "correct");
+      }
+      else
+      {
+            addDivToContainer(arr[x] + " is wrong", "practice-container", "wrong");
       }
 }
-learn();
-
 function test(){
       var count =0;
-      var times = parseInt (window.prompt("Times?"));
+      //var times = parseInt (window.prompt("Times?"));
       var timess = times;
       while (times-- > 0){
             var arr = [];
@@ -54,29 +115,11 @@ function test(){
 }
 
 
-var maximum =0;
 function challenge(){
-      var count =0;
-      var times = 3;
-      
-      while (times > 0){
-            var arr = [];
-            for(var i = 0 ; i<4; i++)
-            { arr.push(quangdb.aWordStartWith(String.fromCharCode(rand(97,122))));
-            }
-            var x = rand(0,3);
-            console.log("Nghia:" + quangdb.getMean(arr[x]));
-            console.log("A: " + arr[0]);
-            console.log("B: " + arr[1]);
-            console.log("C: " + arr[2]);
-            console.log("D: " + arr[3]);
-            var ans = window.prompt(" ");
-            if(check(x, ans)) count++;
-            else times--; 
-      }
-            console.log("Your point is " + count);
-            if (count < maximum){ maximum = count; 
-            console.log("It's new record");
-            }
-            
+      count =0;
+      modePractice = 2;
+
+      timesChallenge = 3;
+      window.alert("Ban duoc sai toi da " + timesChallenge);
+      createQuestion();
 }
